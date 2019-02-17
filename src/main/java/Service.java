41a40b4eef91283d9219325406abf597c22ce357
Nazar Vladyka <nazarvladyka00@gmail.com>
2019-02-17 12:59:37 +0200

    try to write tests without JUnit and others frameworks

---
+++ src/main/java/Service.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Service {
+    public ArrayList<Integer> choseNumbersFromListEqualsToNumber(ArrayList<Integer> inputList, int sumOfResultList) {
+        ArrayList<Integer> resultList = new ArrayList();
+
+        resultList.add(8);
+        resultList.add(5);
+
+        return resultList;
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainTest {
+    public static void main(String[] args) {
+        testChoseNumbersFromListEqualsToNumber();
+    }
+
+    public static void testChoseNumbersFromListEqualsToNumber() {
+        Service service = new Service();
+
+        ArrayList<Integer> testList = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
+        ArrayList<Integer> resultList = service.choseNumbersFromListEqualsToNumber(testList, 8);
+
+        if(resultList.equals(Arrays.asList(5, 8))) {
+            System.out.println("All good");
+        } else if (resultList.equals(Arrays.asList(8, 5))) {
+            System.out.println("Good too");
+        } else {
+            System.err.println("Something went wrong");
+        }
+    }
+}
